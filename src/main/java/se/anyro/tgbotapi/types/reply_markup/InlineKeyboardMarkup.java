da44670eae0b52288cb784a7326eb595c735c5b1
Adam Nybäck <adam@anyro.se>
2016-05-02 13:18:39 +0200

    Added helper methods for single line keyboards (horizontal) and Builder classes for dynamically creating arbitrary keyboards.

--- src/main/java/se/anyro/tgbotapi/types/reply_markup/InlineKeyboardMarkup.java
+++ src/main/java/se/anyro/tgbotapi/types/reply_markup/InlineKeyboardMarkup.java
@@ -1,5 +1,9 @@
 package se.anyro.tgbotapi.types.reply_markup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @see <a href="https://core.telegram.org/bots/api#inlinekeyboardmarkup">Official documentation of
  *      InlineKeyboardMarkup</a>
@@ -13,17 +17,6 @@ public class InlineKeyboardMarkup extends ReplyMarkup {
 
     public InlineKeyboardMarkup(InlineKeyboardButton[]... buttonRows) {
         inline_keyboard = buttonRows;
-    }
-
-    /**
-     * Simplified constructor for one button per row.
-     */
-    public InlineKeyboardMarkup(InlineKeyboardButton... buttons) {
-        int numButtons = buttons.length;
-        inline_keyboard = new InlineKeyboardButton[numButtons][];
-        for (int i = 0; i < numButtons; ++i) {
-            inline_keyboard[i] = new InlineKeyboardButton[] { buttons[i] };
-        }
     }
 
     /**
@@ -43,14 +36,91 @@ public class InlineKeyboardMarkup extends ReplyMarkup {
     }
 
     /**
-     * One button per line from strings.
+     * Creates an InlineKeyboardMarkup with one button per line.
      */
-    public InlineKeyboardMarkup(String... buttonTexts) {
+    public static InlineKeyboardMarkup createVertical(InlineKeyboardButton... buttons) {
+        int numButtons = buttons.length;
+        InlineKeyboardButton[][] keyboard = new InlineKeyboardButton[numButtons][];
+        for (int i = 0; i < numButtons; ++i) {
+            keyboard[i] = new InlineKeyboardButton[] { buttons[i] };
+        }
+        return new InlineKeyboardMarkup(keyboard);
+    }
+
+    /**
+     * Creates an InlineKeyboardMarkup with one button per line from strings. The buttons will have the callback data
+     * set to the same string as the button text.
+     */
+    public static InlineKeyboardMarkup createVertical(String... buttonTexts) {
         int numRows = buttonTexts.length;
-        inline_keyboard = new InlineKeyboardButton[numRows][];
+        InlineKeyboardButton[][] keyboard = new InlineKeyboardButton[numRows][];
         for (int i = 0; i < numRows; ++i) {
             InlineKeyboardButton button = InlineKeyboardButton.callbackData(buttonTexts[i], buttonTexts[i]);
-            inline_keyboard[i] = new InlineKeyboardButton[] { button };
+            keyboard[i] = new InlineKeyboardButton[] { button };
+        }
+        return new InlineKeyboardMarkup(keyboard);
+    }
+
+    /**
+     * Create an InlineKeyboardMarkup with just one row of buttons.
+     */
+    public static InlineKeyboardMarkup createHorizontal(InlineKeyboardButton... buttons) {
+        int numButtons = buttons.length;
+        InlineKeyboardButton[][] keyboard = new InlineKeyboardButton[1][numButtons];
+        for (int i = 0; i < numButtons; ++i) {
+            keyboard[0][i] = buttons[i];
+        }
+        return new InlineKeyboardMarkup(keyboard);
+    }
+
+    /**
+     * Class for dynamically building a keyboard line by line. Call addRow() to add a new row and then call the
+     * add*Button() methods to add buttons to that row. Finally call toMarkup() to create the InlineKeyboardMarkup
+     * object.
+     */
+    public static class Builder {
+
+        public List<List<InlineKeyboardButton>> keyboard = new ArrayList<List<InlineKeyboardButton>>();
+
+        public Builder addRow() {
+            keyboard.add(new ArrayList<InlineKeyboardButton>());
+            return this;
+        }
+
+        public Builder addRow(InlineKeyboardButton... buttons) {
+            keyboard.add(Arrays.asList(buttons));
+            return this;
+        }
+
+        public Builder addButton(InlineKeyboardButton button) {
+            if (keyboard.size() == 0) {
+                addRow();
+            }
+            keyboard.get(keyboard.size() - 1).add(button);
+            return this;
+        }
+
+        public Builder addUrlButton(String text, String url) {
+            return addButton(InlineKeyboardButton.url(text, url));
+        }
+
+        public Builder addCallbackDataButton(String text, String data) {
+            return addButton(InlineKeyboardButton.callbackData(text, data));
+        }
+
+        public Builder addSwitchInlineQueryButton(String text, String query) {
+            return addButton(InlineKeyboardButton.switchInlineQuery(text, query));
+        }
+
+        public InlineKeyboardMarkup toMarkup() {
+            int numRows = this.keyboard.size();
+            InlineKeyboardButton[][] keyboard = new InlineKeyboardButton[numRows][];
+            for (int i = 0; i < numRows; ++i) {
+                List<InlineKeyboardButton> row = this.keyboard.get(i);
+                int numButtons = row.size();
+                keyboard[i] = row.toArray(new InlineKeyboardButton[numButtons]);
+            }
+            return new InlineKeyboardMarkup(keyboard);
         }
     }
 }
--- src/main/java/se/anyro/tgbotapi/types/reply_markup/ReplyKeyboardMarkup.java
+++ src/main/java/se/anyro/tgbotapi/types/reply_markup/ReplyKeyboardMarkup.java
@@ -1,5 +1,9 @@
 package se.anyro.tgbotapi.types.reply_markup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @see <a href="https://core.telegram.org/bots/api#replykeyboardmarkup">Official documentation of
  *      ReplyKeyboardMarkup</a>
@@ -13,17 +17,6 @@ public class ReplyKeyboardMarkup extends ReplyMarkup {
 
     public ReplyKeyboardMarkup(KeyboardButton[]... buttonRows) {
         keyboard = buttonRows;
-    }
-
-    /**
-     * Simplified constructor for one button per row.
-     */
-    public ReplyKeyboardMarkup(KeyboardButton... buttons) {
-        int numButtons = buttons.length;
-        keyboard = new KeyboardButton[numButtons][];
-        for (int i = 0; i < numButtons; ++i) {
-            keyboard[i] = new KeyboardButton[] { buttons[i] };
-        }
     }
 
     /**
@@ -43,13 +36,103 @@ public class ReplyKeyboardMarkup extends ReplyMarkup {
     }
 
     /**
-     * One button per line from strings.
+     * Create ReplyKeyboardMarkup with one button per row.
      */
-    public ReplyKeyboardMarkup(String... buttonTexts) {
+    public static ReplyKeyboardMarkup createVertical(KeyboardButton... buttons) {
+        int numRows = buttons.length;
+        KeyboardButton[][] keyboard = new KeyboardButton[numRows][];
+        for (int i = 0; i < numRows; ++i) {
+            keyboard[i] = new KeyboardButton[] { buttons[i] };
+        }
+        return new ReplyKeyboardMarkup(keyboard);
+    }
+
+    /**
+     * Create ReplyKeyboardButton with one button per line from strings.
+     */
+    public static ReplyKeyboardMarkup createVertical(String... buttonTexts) {
         int numRows = buttonTexts.length;
-        keyboard = new KeyboardButton[numRows][];
+        KeyboardButton[][] keyboard = new KeyboardButton[numRows][];
         for (int i = 0; i < numRows; ++i) {
             keyboard[i] = new KeyboardButton[] { new KeyboardButton(buttonTexts[i]) };
+        }
+        return new ReplyKeyboardMarkup(keyboard);
+    }
+
+    /**
+     * Create a ReplyKeyboardMarkup with just one row of buttons.
+     */
+    public static ReplyKeyboardMarkup createHorizontal(KeyboardButton... buttons) {
+        int numButtons = buttons.length;
+        KeyboardButton[][] keyboard = new KeyboardButton[1][numButtons];
+        for (int i = 0; i < numButtons; ++i) {
+            keyboard[0][i] = buttons[i];
+        }
+        return new ReplyKeyboardMarkup(keyboard);
+    }
+
+    /**
+     * Create a ReplyKeyboardMarkup with just one row of buttons from strings.
+     */
+    public static ReplyKeyboardMarkup createHorizontal(String... buttonTexts) {
+        int numButtons = buttonTexts.length;
+        KeyboardButton[][] keyboard = new KeyboardButton[1][numButtons];
+        for (int i = 0; i < numButtons; ++i) {
+            keyboard[0][i] = new KeyboardButton(buttonTexts[i]);
+        }
+        return new ReplyKeyboardMarkup(keyboard);
+    }
+
+    /**
+     * Class for dynamically building a keyboard line by line. Call addRow() to add a new row and then call the
+     * addButton methods to add buttons to that row. Finally call toMarkup() to create the ReplyKeyboardMarkup object.
+     */
+
+    public static class Builder {
+
+        public List<List<KeyboardButton>> keyboard = new ArrayList<List<KeyboardButton>>();
+
+        public Builder addRow() {
+            keyboard.add(new ArrayList<KeyboardButton>());
+            return this;
+        }
+
+        public Builder addRow(KeyboardButton... buttons) {
+            keyboard.add(Arrays.asList(buttons));
+            return this;
+        }
+
+        public Builder addButton(KeyboardButton button) {
+            keyboard.get(keyboard.size() - 1).add(button);
+            return this;
+        }
+
+        public Builder addButton(String text) {
+            addButton(new KeyboardButton(text));
+            return this;
+        }
+
+        public Builder addContactButton(String text) {
+            KeyboardButton button = new KeyboardButton(text);
+            button.request_contact = true;
+            return addButton(button);
+        }
+
+        public Builder addLocationButton(String text) {
+            KeyboardButton button = new KeyboardButton(text);
+            button.request_location = true;
+            return addButton(button);
+        }
+
+        public ReplyKeyboardMarkup toMarkup() {
+            int numRows = this.keyboard.size();
+            KeyboardButton[][] keyboard = new KeyboardButton[numRows][];
+            for (int i = 0; i < numRows; ++i) {
+                List<KeyboardButton> row = this.keyboard.get(i);
+                int numButtons = row.size();
+                keyboard[i] = row.toArray(new KeyboardButton[numButtons]);
+            }
+            return new ReplyKeyboardMarkup(keyboard);
         }
     }
 }
