8a32302cd79a2879e8df37cb6d78d2491f1cc16a
Adam Nybäck <adam@anyro.se>
2016-05-07 21:46:20 +0200

    Helper methods for creating specific button types

--- src/main/java/se/anyro/tgbotapi/types/reply_markup/KeyboardButton.java
+++ src/main/java/se/anyro/tgbotapi/types/reply_markup/KeyboardButton.java
@@ -8,10 +8,26 @@ package se.anyro.tgbotapi.types.reply_markup;
 public class KeyboardButton {
 
     public String text;
-    public boolean request_contact;
-    public boolean request_location;
+    public Boolean request_contact;
+    public Boolean request_location;
 
     public KeyboardButton(String text) {
         this.text = text;
+    }
+
+    public static KeyboardButton textButton(String text) {
+        return new KeyboardButton(text);
+    }
+
+    public static KeyboardButton contactButton(String text) {
+        KeyboardButton button = new KeyboardButton(text);
+        button.request_contact = true;
+        return button;
+    }
+
+    public static KeyboardButton locationButton(String text) {
+        KeyboardButton button = new KeyboardButton(text);
+        button.request_location = true;
+        return button;
     }
 }
